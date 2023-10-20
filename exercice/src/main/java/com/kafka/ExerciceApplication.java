2d46367de082d5bb60d5962ff4300eea06f01ff9
antoinejanvrot <antoine.janvrot@carbon-it.com>
2023-10-20 17:31:10 +0200

    first

---
+++ exercice/src/main/java/com/kafka/ExerciceApplication.java
@@ -0,0 +1,11 @@
+package com.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciceApplication.class);
+    }
+}
